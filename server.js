cc2ea406868860d5beb2c8b0e5e065dcf935b172
adonaivillalobos <vil21022@byui.edu>
2025-03-08 19:55:58 +0100

    server.js change

--- server.js
+++ server.js
@@ -6,39 +6,42 @@
 /* ***********************
  * Require Statements
  *************************/
-const express= require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const env = require("dotenv").config();
+const path = require("path");
+const app = express();
+const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") //not at views root
+// Explicitly set views directory
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // Not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
 // Index Route
-app.get("/",  function(req, res){
-  res.render("index" , { title: "Home" })
-})
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
