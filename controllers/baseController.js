718f2450fc99717c1dff7e726e1b96b545d346a7
adonaivillalobos <vil21022@byui.edu>
2025-03-25 21:23:25 +0100

    double and triple check my  original code

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,18 +1,9 @@
-const utilities = require("../utilities/") // Ensure correct import
-
+const utilities = require("../utilities/")
 const baseController = {}
 
-/* ***************************
- *  Build Home Page
- * ************************** */
-baseController.buildHome = async function (req, res) {
-  try {
-    const nav = await utilities.getNav() // Ensure function name matches
-    res.render("index", { title: "Home", nav })
-  } catch (error) {
-    console.error("Error in buildHome:", error)
-    res.status(500).send("Internal Server Error")
-  }
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
 }
 
 module.exports = baseController
